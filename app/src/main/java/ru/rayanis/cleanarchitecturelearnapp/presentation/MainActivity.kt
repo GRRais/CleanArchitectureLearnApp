9cb90e7aa81dd26c9f1dcd65a335cc8dd402e8f8
Rais Garifzyanov <garifzyanov.rr@gmail.com>
2022-12-16 09:50:43 +0300

    added MVVM

--- app/src/main/java/ru/rayanis/cleanarchitecturelearnapp/presentation/MainActivity.kt
+++ app/src/main/java/ru/rayanis/cleanarchitecturelearnapp/presentation/MainActivity.kt
@@ -1,43 +1,40 @@
 package ru.rayanis.cleanarchitecturelearnapp.presentation
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import ru.rayanis.cleanarchitecturelearnapp.data.repository.UserRepositoryImpl
-import ru.rayanis.cleanarchitecturelearnapp.data.storage.sharedprefs.SharedPrefUserStorage
+import androidx.lifecycle.ViewModelProvider
 import ru.rayanis.cleanarchitecturelearnapp.databinding.ActivityMainBinding
-import ru.rayanis.cleanarchitecturelearnapp.domain.usecase.GetUserNameUseCase
-import ru.rayanis.cleanarchitecturelearnapp.domain.usecase.SaveUserNameUseCase
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var b: ActivityMainBinding
 
-    private val userRepository by lazy(LazyThreadSafetyMode.NONE) {
-        UserRepositoryImpl(userStorage = SharedPrefUserStorage(context = applicationContext))
-    }
-    private val getUserNameUseCase by lazy(LazyThreadSafetyMode.NONE) {
-        GetUserNameUseCase(userRepository = userRepository)
-    }
-    private val saveUserNameUseCase by lazy(LazyThreadSafetyMode.NONE) {
-        SaveUserNameUseCase(userRepository = userRepository)
-    }
+    private lateinit var vm: MainViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         b = ActivityMainBinding.inflate(layoutInflater)
         setContentView(b.root)
 
+        Log.e("AAA" , "Activity created")
+
+        vm = ViewModelProvider(
+            this ,
+            MainViewModelFactory(this)
+        )[MainViewModel::class.java]
+
+        vm.resultLive.observe(this) {
+            b.dataTextView.text = it
+        }
+
         b.sendButton.setOnClickListener {
             val text = b.dataEditView.text.toString()
-            val params =
-                ru.rayanis.cleanarchitecturelearnapp.domain.models.SaveUserNameParam(name = text)
-            val result = saveUserNameUseCase.execute(param = params)
-            b.dataTextView.text = "Save result = $result"
+            vm.save(text)
         }
 
         b.receiveButton.setOnClickListener {
-            val userName = getUserNameUseCase.execute()
-            b.dataTextView.text = "${userName.firstName} ${userName.lastName}"
+            vm.load()
         }
     }
 }
